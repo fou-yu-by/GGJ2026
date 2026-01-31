e7c56e2997c663487765270b7dd27b3a9717b0bb
WANGYUXUAN2004\86151 <406323523@qq.com>
2026-01-31 15:11:25 +0800

    test

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -72,7 +72,7 @@ public class MainPlayer : MonoBehaviour
         //hits里的每一个enemy执行受伤逻辑
         foreach (var enemy in hits)
         {
-            enemy.transform.GetComponent<Monster>().TakeDamage(playerStats.damage.GetValue());
+            enemy.transform.GetComponent<Monster>()?.TakeDamage(playerStats.damage.GetValue());
         }
         
     }
@@ -106,7 +106,6 @@ public class MainPlayer : MonoBehaviour
         {
             EquipManager.Instance.Mask = mask;
             EquipManager.Instance.UpdateSlotUI();
-            Debug.Log(EquipManager.Instance.equipmentDict);
         }
     }
 
--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections;
+
 using UnityEngine.UI;
 public class PlayerStats : CharacterStats
 {
