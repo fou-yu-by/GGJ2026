73c1c5986f551fe43aab4da4634e415fde0b1088
HaoHaoLucas <2313583830@qq.com>
2026-02-01 16:17:01 +0800

    1

--- Assets/Scripts/Player/Skill/A.cs
+++ Assets/Scripts/Player/Skill/A.cs
@@ -8,7 +8,11 @@ public class A : MonoBehaviour
 
 	private float flySpeed;
 	[SerializeField] private LayerMask enemyLayer;
+<<<<<<< Updated upstream
 	private int damage = 10;
+=======
+	private int damage=10;
+>>>>>>> Stashed changes
 	private Vector3 direction;
 
 	private void Awake()
